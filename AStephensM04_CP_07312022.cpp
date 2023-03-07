be9aa37af4b6f00ef89a493913ac0afb97a6c27a
Austin S <austinstephens103@gmail.com>
2023-03-06 23:08:26 -0600

    Add files via upload

---
+++ AStephensM04_CP_07312022.cpp
@@ -0,0 +1,62 @@
+/*
+Author: Austin Stephens
+
+Date: 07/16/2022
+
+FileName: AStephens_M02LA_07162022
+
+Purpous: 
+
+   Bank Simulation
+
+Input: 
+    
+
+Exceptions:
+
+    Bank Simulation
+
+*/
+
+#include "Classes.h"
+#include "Functions.h"
+#include <iostream>
+#include <cstdlib>
+#include <stack>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int pass = 0;
+    
+    srand (time(NULL)); // NTS: include in main, dont call each time
+    
+    
+    
+    
+    Customer *C[20];
+    C[pass] = new Customer;
+    C[pass] -> DataFeed(pass);
+    pass++;
+ 
+
+
+
+
+    while (1)
+    {
+
+        
+    }
+    
+
+
+
+
+
+
+
+    return 0;
+};
